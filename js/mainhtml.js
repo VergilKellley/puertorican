2636ecc73964a3c7554650a074e641ca3c2e04c7
Vergil Kelley <verg_1@yahoo.com>
2018-12-27 10:36:49 -0600

    added lightbox photo gallery js

--- js/mainhtml.js
+++ js/mainhtml.js
@@ -1,7 +1,7 @@
 //header.php image slider
-/*$(document).ready(function(){
-	var imageName = ["img/badge1.jpg", "img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg", "img/img7.jpg", "img/img8.jpg"];
-	var imageName2 = ["img/img11.jpg", "img/img12.jpg", "img/img13.jpg"]
+$(document).ready(function(){
+	var imageName = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg", "img/img7.jpg", "img/img8.jpg"];
+	//var imageName2 = ["img/img11.jpg", "img/img12.jpg", "img/img13.jpg"]
 	var imageTitle = [];
 	var counter = 0;
 
@@ -11,7 +11,7 @@
 			$("#picture").attr("src", imageName[counter]);
 			$("#imageCaption").text(imageTitle[counter]);
 			counter++;
-			if (counter > 8) {counter = 0;}
+			if (counter > 7) {counter = 0;}
 			$("#picture").fadeIn(500).delay(3000);
 			go();// comment this line out to turn into click image slider
 		});
@@ -20,14 +20,14 @@
 	//uncomment below to turn into click image slider
 	//$("#picture").click(clickaway);
 });
-*/
+
 
 //var imageName2 = ["img/img11.jpg", "img/img12.jpg", "img/img13.jpg"];
 //var counter = 0;
 
 
 
-var i = 0;
+/*var i = 0;
 var images =[];
 var time = 3000;
 
