246feab5f7a93763cad9cc4b0bcddd699f62ebde
Vergil Kelley <verg_1@yahoo.com>
2019-01-16 11:07:25 -0600

    added photo to slider

--- js/mainhtml.js
+++ js/mainhtml.js
@@ -1,6 +1,6 @@
 //header.php image slider
 $(document).ready(function(){
-	var imageName = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg", "img/img7.jpg", "img/img8.jpg"];
+	var imageName = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpeg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg", "img/img7.jpg", "img/img8.jpg"];
 	//var imageName2 = ["img/img11.jpg", "img/img12.jpg", "img/img13.jpg"]
 	var imageTitle = [];
 	var counter = 0;
