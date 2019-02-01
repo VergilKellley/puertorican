42bbd68dd1c338819879f46be604a85c78272521
Vergil Kelley <verg_1@yahoo.com>
2019-01-31 20:14:51 -0600

    adjusted slider function

--- js/mainhtml.js
+++ js/mainhtml.js
@@ -22,72 +22,16 @@ $(document).ready(function(){
 });
 
 
-//var imageName2 = ["img/img11.jpg", "img/img12.jpg", "img/img13.jpg"];
-//var counter = 0;
+/********** GET INFO FROM RADIO BUTTONS IN JOIN PRPA **********************/
 
-
-
-/*var i = 0;
-var images =[];
-var time = 3000;
-
-// image list
-images[0] = 'img1.jpg';
-images[1] = 'img2.jpg';
-images[2] = 'img/img3.jpg';
-images[3] = 'img/img4.jpg';
-
-// change image
-function changeImg(){
-	document.photo.src = images[i];
-
-	if(i < images.length -1){
-		i++;
-	} else {
-		i = 0;
-	}
-
-	setTimeout("changeImg()", time);
-}
-
-
-/*$(function go(){
-	$(".picture").click(function() {
-		$(this).fadeOut(300, function(){
-			$(this).attr("src", imageName2[counter]);
-			counter++;
-			if (counter > 3) { counter = 0;}
-			$(this).fadeIn(500).delay(3000);
-			go();
-		})
+$(document).ready(function() {
+	$('#btnSubmit').click(function () {
+		var result = $('input[type = "radio"]:checked');
+		if (result.length > 0) {
+			$('#divResult').html(result.val() + " is checked");
+		}
+		else {
+				$('#divResult').html("Nothing clicked");
+		}
 	});
-});*/
-// continuous slider no click
-/*$(function go(){
-	//$(".picture").click(function() {
-		$(".picture").fadeOut(300, function(){
-			$(".picture").attr("src", imageName2[counter]);
-			counter++;
-			if (counter > 3) { counter = 0;}
-			$(".picture").fadeIn(500).delay(3000);
-			go();
-		})
-	});
-*/
-/*
-var i = 0;
-var imageName2 = ["img/img11.jpg", "img/img12.jpg", "img/img13.jpg"];
-*/
-/*while (i < imageName2.length){
-	console.log("hey");
-	i = i+1;
-}
-
-do {
-	console.log('now');
-	i+1;
-} while (i < imageName2.length);
-*/
-/*for (var i = 0; i < imageName2.length; i++) {
-	console.log('more');
-}*/
+});
